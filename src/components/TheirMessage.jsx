e61a9d8c0525edd8031c31750540ff1ab013e80e
Christopher Johnson <johnsonce@goldmail.etsu.edu>
2021-10-23 12:17:43 -0400

    small style change

--- src/components/TheirMessage.jsx
+++ src/components/TheirMessage.jsx
@@ -19,7 +19,7 @@ const TheirMessage = ({ lastMessage, message }) => {
           />
         )
         : (
-          <div className="message" style={{ float: 'left', color: 'white', backgroundColor: '#575757', marginLeft: isFirstMessageByUser ? '4px' : '48px' }}>
+          <div className="message" style={{ float: 'left', color: 'white', backgroundColor: '#000000', marginLeft: isFirstMessageByUser ? '4px' : '48px' }}>
             {message.text}
           </div>
         )}
